6cf9ead28a470e2d9e10ed2884af8c283961b49f
Rafael Bicalho <rbmbika@gmail.com>
2023-05-02 22:11:30 -0300

    Add syntax highlighting for code blocks

--- next.config.js
+++ next.config.js
@@ -1,8 +1,28 @@
+const rehypePrettyCode = require('rehype-pretty-code')
+
+const options = {
+  theme: 'nord',
+  keepBackground: true,
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and
+    // allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{ type: 'text', value: ' ' }]
+    }
+  },
+  onVisitHighlightedLine(node) {
+    node.properties.className.push('line--highlighted')
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ['word']
+  },
+}
+
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
-    rehypePlugins: [],
+    rehypePlugins: [[rehypePrettyCode, options]],
   },
 })
 
