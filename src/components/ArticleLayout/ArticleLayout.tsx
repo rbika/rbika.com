2e63db95e7af70065278be2717357cc8eef29a4e
Rafael Bicalho <rbmbika@gmail.com>
2025-03-03 22:52:51 -0300

    Update article layout

--- src/components/ArticleLayout/ArticleLayout.tsx
+++ src/components/ArticleLayout/ArticleLayout.tsx
@@ -1,16 +1,24 @@
 import { format } from 'date-fns'
 import PostMetaData from '@/types/postMetaData'
-import ArticleFooter from '../ArticleFooter'
 import PageContainer from '../PageContainer'
 import Seo from '../Seo'
+import profileImage from '@/../public/profile.jpg'
 import Image from 'next/image'
+import { socialItems } from '@/utils/constants'
 
-type ArticleProps = {
+interface ArticleProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode
   meta: PostMetaData
-} & React.HTMLAttributes<HTMLDivElement>
+}
 
 const Article = ({ children, meta }: ArticleProps) => {
+  const handleBackToTopCLick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
   return (
     <PageContainer>
       <Seo
@@ -21,17 +29,35 @@ const Article = ({ children, meta }: ArticleProps) => {
         image={meta.coverImage}
       />
 
-      <header className="mb-12">
+      <header className="mb-4">
         <h1 className="text-4xl leading-tight">{meta.title}</h1>
-        <p className="text-secondary mt-4 mb-8 pb-8 sm:mt-2 text-sm border-b border-zinc-800">
-          {meta?.date ? format(new Date(meta.date), 'MMM d, yyyy') : '---'}
-        </p>
+
+        <div className="flex gap-4 items-center flex-row pb-6 my-6 border-b border-zinc-800">
+          <div className="w-[36px] h-[36px] overflow-hidden rounded-full">
+            <Image
+              width="36"
+              height="36"
+              src={profileImage}
+              alt="profile"
+              placeholder="blur"
+            />
+          </div>
+          <div className="flex flex-col">
+            <p className="text-secondary text-sm">Rafael Bika(s)</p>
+            <p className="text-secondary text-xs">
+              {meta?.date ? format(new Date(meta.date), 'MMM d, yyyy') : '---'}
+            </p>
+          </div>
+        </div>
       </header>
 
-      <div className="text-secondary max-w-none prose dark:prose-invert prose-h2:font-medium">
+      <div className="text-secondary max-w-none prose dark:prose-invert prose-h2:font-medium border-b border-zinc-800 pb-10 mb-10">
         {children}
       </div>
-      <ArticleFooter className="mt-12" />
+
+      <div className="flex justify-center mt-10 text-sm text-secondary">
+        <button onClick={handleBackToTopCLick}>↑ Back to top</button>
+      </div>
     </PageContainer>
   )
 }
